e30e1bb0ef7f060d34e79a020dd35d16bdd6771e
Muhammed Thanish <mnmtanish@gmail.com>
2017-02-25 18:43:43 +0530

    Improve test spy helpers

--- src/__test_utils.ts
+++ src/__test_utils.ts
@@ -1,9 +1,38 @@
 /**
- * spyOn
- * ...
+ * SPIES
+ * SPIES is a symbol field used to store spied method information.
  */
-export const spyOn = ( ctx: any, method: string ): jest.Mock<any> => {
-    const fn = jest.fn();
-    ctx[ method ] = fn;
-    return fn;
+const SPIES = Symbol();
+
+/**
+ * spyOff
+ * spyOff replaces a spied method with the original method.
+ */
+export const spyOff = ( ctx: any, method?: string ) => {
+    if ( !method ) {
+        Object.keys( ctx ).forEach( key => spyOff( ctx, key ));
+        return;
+    }
+    if ( !ctx[ SPIES ] || !ctx[ SPIES ][ method ]) {
+        return;
+    }
+    ctx[ method ] = ctx[ SPIES ][ method ].real;
+};
+
+/**
+ * spyOn
+ * spyOn replaces a method with a jest spy and returns the spy.
+ */
+export const spyOn = ( ctx: any, method: string, fake?: Function ): jest.Mock<any> => {
+    if ( !ctx[ SPIES ]) {
+        ctx[ SPIES ] = {};
+    }
+    if ( ctx[ SPIES ][ method ]) {
+        return ctx[ SPIES ][ method ].spy;
+    }
+    const real = ctx[ method ];
+    const spy = jest.fn( fake );
+    ctx[ SPIES ][ method ] = { real, spy };
+    ctx[ method ] = spy;
+    return spy;
 };
