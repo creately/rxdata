2cd8696ec5d35fc98076f2ea821a8342f440c50c
Muhammed Thanish <mnmtanish@gmail.com>
2017-08-22 10:59:52 +0530

    Fix lint errors with prettier

--- src/tests.ts
+++ src/tests.ts
@@ -1,3 +1,3 @@
 declare var require: any;
-const context = require.context( './', true, /\.spec\.ts$/ );
-context.keys().map( context );
+const context = require.context('./', true, /\.spec\.ts$/);
+context.keys().map(context);
