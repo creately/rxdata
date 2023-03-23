49224bb40f014f73b833b3d8467c308e974ac0e1
thisunravisara <thisunravisara@gmail.com>
2023-03-23 16:05:10 +0530

    Updated inser

--- src/__tests__/collection.spec.ts
+++ src/__tests__/collection.spec.ts
@@ -351,12 +351,12 @@ describe('Collection', () => {
       const promise2 = watchN(col, 1);
       await col.insert(Object.freeze({ id: 'd111', x: 2, y: 2, z: 2 }));
       out = await promise2;
-      expect(( col as any ).storage.data.length ).toEqual( TEST_DOCS.length );
-      expect(( col as any ).storage.data.filter(( doc: any ) => doc.id === 'd111' ).length ).toEqual( 1 );
-      const updated = ( col as any ).storage.data.find(( doc: any ) => doc.id === 'd111' )
-      expect( updated.x ).toEqual( 2 );
-      expect( updated.y ).toEqual( 2 );
-      expect( updated.z ).toEqual( 2 );
+      expect((col as any).storage.data.length).toEqual(TEST_DOCS.length);
+      expect((col as any).storage.data.filter((doc: any) => doc.id === 'd111').length).toEqual(1);
+      const updated = (col as any).storage.data.find((doc: any) => doc.id === 'd111');
+      expect(updated.x).toEqual(2);
+      expect(updated.y).toEqual(2);
+      expect(updated.z).toEqual(2);
       done();
     });
 
